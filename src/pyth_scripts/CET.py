ae22132eac1277eefcc77d8404fbd55641bc1352
Rex L Baum <baum@usgs.gov>
2016-01-29 18:20:04 -0700

    Add updated command files and scripts for plotting extensometer data

---
+++ src/pyth_scripts/CET.py
@@ -0,0 +1,80 @@
+#CET.py
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+import csv
+from numpy import ma
+from matplotlib.dates import strpdate2num
+
+def readfiles(file_list,c1):
+    data = []
+    for fname in file_list:
+        data.append(
+                    np.loadtxt(fname,
+                               usecols=(0,c1),
+                               comments='#',    # skip comment lines
+                               delimiter='\t',
+                               converters = { 0 : strpdate2num('%Y-%m-%d %H:%M:%S') },
+                               dtype=None))
+    return data
+
+
+data = readfiles(['waWat_DynCont_14d.txt'],9) # 9
+data_1 = ma.fix_invalid(data, fill_value = 'nan')
+
+column_0 = np.array(data_1)[0][:,0]
+cetDistance_raw = np.array(data_1)[0][:,1]
+
+#Const cet_mult = 0.6477' mm/mV
+#Const cet_offs = -14
+cetDistance_mult = 0.6477
+cetDistance_Offs = -14
+
+cetDistance_mm = cetDistance_raw*cetDistance_mult+cetDistance_Offs
+#cetDistance_m = cetDistance_mm/1000.
+
+def init_plot(title, yMin=-1, yMax=40):
+    plt.figure(figsize=(12, 6)) # figsize=(24, 12)
+    plt.title(title + disclamers, fontsize=11)
+    plt.xlabel(xtext)
+    plt.ylabel(ytext)
+    #plt.xlim(xMin,xMax)
+    plt.ylim(yMin,yMax)
+    plt.grid()
+    #plt.xticks(np.arange(xMin,xMax+1))
+
+def end_plot(name=None, cols=5):
+    plt.legend(bbox_to_anchor=(0, -.15, 1, -0.5), loc=8, ncol=cols, fontsize=10,
+               mode="expand", borderaxespad=-1.,  scatterpoints=1)
+    if name:
+        plt.savefig(name, bbox_inches='tight')
+
+disclamers = ('\nUSGS PROVISIONAL DATA'
+              '\nSUBJECT TO REVISION'
+              )
+xtext = ('Date & Time')
+ytext = ('Extensometer distance, mm')
+
+# Set fontsize for plots
+
+font = {'family' : 'monospace',
+    'weight' : 'normal',
+        'size'   : '10'}
+
+matplotlib.rc('font', **font)  # pass in the font dict as kwargs
+
+init_plot('Extensometer Distance at ALS')
+
+plt.plot(column_0, cetDistance_mm, linestyle='-', color='b', label='CET 1')
+
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d\n%H:%M'))
+plt.gca().xaxis.set_major_locator(mdates.HourLocator())
+plt.gca().xaxis.set_minor_locator(mdates.HourLocator(interval=6))
+plt.gca().xaxis.set_major_locator(mdates.DayLocator(interval=1))
+
+end_plot(name='MVWat_Dyn_cet.png')
+
--- src/pyth_scripts/Readme.txt
+++ src/pyth_scripts/Readme.txt
@@ -3,3 +3,6 @@ PATH=~/anaconda/bin:$PATH
 
 
 python <scriptname>.py
+
+
+Note:  On 1/14/2016, I observed that the forecast plots for AWI and id_index do not match corresponding heights for AWI & threshold index on corresponding dates in the history only plots (NWS folder).  Differences between how plotting is coded between Forecast.py and NWS.py seem slight, but might explain the differences(?)  Forecast.py uses a slightly different approach, ignoring the markers tuple in order to use different colors to distinguish the forecast values from observed values, whereas NWS.py uses markers to make the plots.
