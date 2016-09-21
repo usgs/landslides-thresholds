c6f4e9c6db6f90dcffd73803fd9b33af71733b74
Rex L Baum <baum@usgs.gov>
2016-09-21 14:42:47 -0600

    Corrections to roc.sh and added data to rainfall input files  and corresponding dates to landslide dates file.

--- src/roc.sh
+++ src/roc.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 cd  ../data/test/ 
 # Match slide dates to cumulative precip output
-grep -f dates2011Muk.txt ./output/3-15/ThArchive02.txt > ./3-15/slides_cum.txt
-# Extract and sort hourly values
-grep -v [a-z] ./output/3-15/ThArchive02.txt | awk '{print $3}' | grep -v 99.00 | sort -g > ./3-15/hourly_sort.txt
+grep -f dates2011Muk.txt ./data/ThArchive02.txt > ./3-15/slides_cum.txt
+# Extract and sort 72-hour values
+grep -v [a-z] ./data/ThArchive02.txt | awk '{print $5}' | grep -v 99.00 | sort -g > ./3-15/72hour_sort.txt
 # Extract and sort AWI hourly values
-grep -v [a-z] ./output/3-15/ThArchive02.txt | awk '{print $9}' | grep -v 99.00 | sort -g > ./3-15/awi_sort.txt
+grep -v [a-z] ./data/ThArchive02.txt | awk '{print $9}' | grep -v 99.00 | sort -g > ./3-15/awi_sort.txt
 # For slide dates only, collect data needed to define cumulative distributions
-# Extract and sort hourly values
-grep -v [a-z] ./3-15/slides_cum.txt | awk '{print $3}' | grep -v 99.00 | sort -g > ./3-15/hourly_sort_sl.txt
+# Extract and sort 72-hour values
+grep -v [a-z] ./3-15/slides_cum.txt | awk '{print $5}' | grep -v 99.00 | sort -g > ./3-15/72hour_sort_sl.txt
 # Extract and sort AWI hourly values
 grep -v [a-z] ./3-15/slides_cum.txt | awk '{print $9}' | grep -v 99.00 | sort -g > ./3-15/awi_sort_sl.txt
 # Compute Receiver Operating Characteristics statistics 
-../../bin/tsthresh > ../data/test/3-15/roc_output.txt
+../../bin/tsthresh > ./3-15/roc_output.txt
 exit
